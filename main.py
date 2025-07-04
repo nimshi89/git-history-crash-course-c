5c9f1847dfab539616127fa867dc8701962f0946
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-07-04 14:52:25 +0100

    Make a list of secrets instead of manual prints.

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ load_dotenv()
 
 if __name__ == "__main__":
 
+    secrets_list = []
+
     aws_secret = REDACTED
 
-    print(f"Accessed secret: {aws_secret}")
+    secrets_list.append(aws_secret)
 
     # Access the secret from the environment variable
     secret = os.getenv("SECRET_KEY")
 
     if secret:
-        print(f"Secret from environment: {secret}")
+        secrets_list.append(secret)
     else:
-        print("No secret found in environment variables.")
+        print("No secret found in .env file.")
+
+    # Print the secrets
+    for i, secret in enumerate(secrets_list):
+        print(f"Secret {i+1}: {secret}")
         
