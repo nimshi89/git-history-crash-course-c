b719bd1aae0852b419ecd1d74e9cac9f94f1e9ee
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-07-04 14:50:23 +0100

    Add .env support

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 if __name__ == "__main__":
-    # Access the secret from the environment variable
+
     aws_secret = REDACTED
 
     print(f"Accessed secret: {aws_secret}")
+
+    # Access the secret from the environment variable
+    secret = os.getenv("SECRET_KEY")
+
+    if secret:
+        print(f"Secret from environment: {secret}")
+    else:
+        print("No secret found in environment variables.")
         
